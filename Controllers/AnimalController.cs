a8227bb626b15089e39eb05dc87b0e6173ca7638
alinabzykova <bzykovaaa@gmail.com>
2026-05-10 16:00:40 +0300

    добавлены вьюшки главной страницы, каталога и описание каждой собаки

---
+++ Controllers/AnimalController.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShelterSiteNET.Models;
+using System.Linq;
+
+namespace ShelterSiteNET.Controllers
+{
+    public class AnimalController : Controller
+    {
+        public IActionResult Index()
+        {
+            var animals = new List<Animal>
+            {
+                new Animal
+                {
+                    Id = 1,
+                    Name = "Бобик",
+                    Age = 4,
+                    Breed = "Лабрадор",
+                    Size = "Большой",
+                    Images = new List<string>
+                    {
+                        "/images/dogs/Бобик 1.jpg"
+                    }
+                },
+
+                new Animal
+                {
+                    Id = 2,
+                    Name = "Джек",
+                    Age = 5,
+                    Breed = "Корги",
+                    Size = "Большой",
+                    Images = new List<string>
+                    {
+                        "/images/dogs/Джек 1.jpg"
+                    }
+                },
+
+                new Animal
+                {
+                    Id = 3,
+                    Name = "Лайка",
+                    Age = 5,
+                    Breed = "Корги",
+                    Size = "Средний",
+                    Images = new List<string>
+                    {
+                        "/images/dogs/Лайка 1.jpg"
+                    }
+                },
+
+                new Animal
+                {
+                    Id = 3,
+                    Name = "Тузик",
+                    Age = 5,
+                    Breed = "Корги",
+                    Size = "Средний",
+                    Images = new List<string>
+                    {
+                        "/images/dogs/Тузик 1.jpg"
+                    }
+                }
+            };
+
+            return View(animals);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var animals = new List<Animal>
+            {
+                new Animal
+                {
+                    Id = 1,
+                    Name = "Бобик",
+                    Age = 4,
+                    Breed = "Лабрадор",
+                    Size = "Большой",
+                    Images = new List<string>
+                    {
+                        "/images/dogs/Бобик 1.jpg"
+                    }
+                },
+
+                new Animal
+                {
+                    Id = 2,
+                    Name = "Джек",
+                    Age = 5,
+                    Breed = "Корги",
+                    Size = "Большой",
+                    Images = new List<string>
+                    {
+                        "/images/dogs/Джек 1.jpg"
+                    }
+                },
+
+                new Animal
+                {
+                    Id = 3,
+                    Name = "Лайка",
+                    Age = 5,
+                    Breed = "Корги",
+                    Size = "Средний",
+                    Images = new List<string>
+                    {
+                        "/images/dogs/Лайка 1.jpg"
+                    }
+                },
+
+                new Animal
+                {
+                    Id = 3,
+                    Name = "Тузик",
+                    Age = 5,
+                    Breed = "Корги",
+                    Size = "Средний",
+                    Images = new List<string>
+                    {
+                        "/images/dogs/Тузик 1.jpg"
+                    }
+                }
+            };
+
+            var animal = animals.FirstOrDefault(a => a.Id == id);
+
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            return View(animal);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
