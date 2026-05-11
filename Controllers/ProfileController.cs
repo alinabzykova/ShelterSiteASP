e6b4f4b5b74f789e0931466217d93e8227da463f
amina-2007 <aminagatagova@mail.ru>
2026-05-11 19:16:05 +0300

    пока накидали профиль

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShelterSiteNET.Models;
+
+namespace ShelterSiteASP.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new UserProfileViewModel
+            {
+                User = new User(),
+                FavoriteAnimals = new List<Animal>()
+            };
+
+            return View(model); //пустая чтоб загружать страниц у промтоиыгыгшдвпмлрым
+        }
+    }
+}
