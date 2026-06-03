deb083303f2c52ff831a4a3a0c9099bbc35b33d4
alinabzykova <bzykovaaa@gmail.com>
2026-06-03 10:08:42 +0300

    Добавлено пожертвование (донат)

---
+++ Controllers/DonationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShelterSiteNET.Controllers
+{
+    public class DonationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string donorName, decimal amount)
+        {
+            ViewBag.Success = true;
+            ViewBag.Name = donorName;
+            ViewBag.Amount = amount;
+
+            return View();
+        }
+    }
+}
